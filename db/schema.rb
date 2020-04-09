23dfd77cd16bed420b0d3f3195ca30939dbaef6f
“Perla <perla@gurucomm.mx>
2020-04-09 13:26:26 -0500

    Add

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_183503) do
+ActiveRecord::Schema.define(version: 2020_04_09_181130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "api_keys", force: :cascade do |t|
     t.string "api_id"
